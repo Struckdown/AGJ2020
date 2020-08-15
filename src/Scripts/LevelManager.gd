639d3ce0798a33c7b1decc3d1ab1fb57d7f60607
Struckdown <bfleysher@gmail.com>
2020-08-15 17:35:44 -0600

    Colored the world mechanic

--- src/Scripts/LevelManager.gd
+++ src/Scripts/LevelManager.gd
@@ -1,13 +1,20 @@
 extends Node
 
 var objectsColored = 0
-var winAmount = 3
+export(int) var partsBetweenUpdates = 5
+export(int) var totalAmountOfUpdates = 3
+
 export(NodePath) var UI_Path
 var UI
+export(NodePath) var tileMap_Path
+var tileMap
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	UI = get_node(UI_Path)
+	tileMap = get_node(tileMap_Path)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -18,6 +25,13 @@ func objectColored():
 	objectsColored += 1
 	UI.score += 1
 	UI.update()
+	if objectsColored % partsBetweenUpdates == 0:
+		updateTilemap()
 	GlobalMusicPlayer.transitionToNextSong()
-	if objectsColored >= winAmount:
+	if objectsColored >= partsBetweenUpdates*totalAmountOfUpdates:
 		var _error = get_tree().change_scene("res://Scenes/VictoryLevel.tscn")
+
+func updateTilemap():
+	var colorFraction = min(1, float(objectsColored) / float(totalAmountOfUpdates*partsBetweenUpdates))
+	tileMap.material.set_shader_param("ColorLevel", colorFraction)
+
