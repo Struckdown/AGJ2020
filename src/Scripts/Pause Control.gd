9b5c91c6587cc17b2fe53886d037c9486c646f23
David Estes McKnight <belmont95@live.com>
2020-08-16 07:03:19 -0500

    Adding sound effects for mouseover effects for the pause menu

--- src/Scripts/Pause Control.gd
+++ src/Scripts/Pause Control.gd
@@ -1,11 +1,14 @@
-extends Control
+extends Node
+export(NodePath) var top_level_control_node
+onready var top_level_control: Control = (get_node(top_level_control_node) as Control)
 export(NodePath) var continue_button_node
 onready var continue_button: Button = (get_node(continue_button_node) as Button)
 export(NodePath) var main_menu_button_node
 onready var main_menu_button: Button = (get_node(main_menu_button_node) as Button)
 export(NodePath) var application_close_button_node
 onready var application_close_button: Button = (get_node(application_close_button_node) as Button)
-export(NodePath) var menu_sound_node
+export(NodePath) var mouseover_soundeffect_node
+onready var mouseover_soundeffect: AudioStreamPlayer = (get_node(mouseover_soundeffect_node) as AudioStreamPlayer)
 export(String, FILE) var main_menu_scene
 
 # Declare member variables here. Examples:
@@ -15,10 +18,17 @@ export(String, FILE) var main_menu_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.visible=false
+	top_level_control.visible=false
+	
+	#Connect button functionality
 	continue_button.connect("pressed", self, "_pause_toggle", [false])
 	main_menu_button.connect("pressed", self, "_main_menu_switch")
 	application_close_button.connect("pressed", self, "_quit_application")
+	
+	#Connect button sound effects
+	continue_button.connect("mouse_entered", self, "_button_mouseover_sfx")
+	main_menu_button.connect("mouse_entered", self, "_button_mouseover_sfx")
+	application_close_button.connect("mouse_entered", self, "_button_mouseover_sfx")	
 	pass # Replace with function body.
 
 
@@ -28,7 +38,7 @@ func _process(delta):
 		_pause_toggle(not get_tree().paused)
 
 func _pause_toggle(on: bool) -> void:
-	self.visible = on
+	top_level_control.visible = on
 	get_tree().paused = on
 
 func _main_menu_switch() -> void:
@@ -36,3 +46,6 @@ func _main_menu_switch() -> void:
 
 func _quit_application() -> void:
 	get_tree().quit()
+
+func _button_mouseover_sfx() -> void:
+	mouseover_soundeffect.play()
