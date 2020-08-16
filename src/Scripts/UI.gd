d3aa39962d69b3861ea4539b34908f9d207e76f0
David Estes McKnight <belmont95@live.com>
2020-08-16 09:46:42 -0500

    Updating UI to have a panelled background

--- src/Scripts/UI.gd
+++ src/Scripts/UI.gd
@@ -3,6 +3,10 @@ extends Control
 var score = 0
 export(NodePath) var time_node
 onready var timer: Node = (get_node(time_node) as Node)
+export(NodePath) var time_label_node
+onready var time_label := get_node(time_label_node)
+export(NodePath) var points_label_node
+onready var points_label := get_node(points_label_node)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,5 +19,5 @@ func _process(delta):
 
 
 func update():
-	$CanvasLayer/Label.text = "Points:" + str(score)
-	$CanvasLayer/Time.text = str(timer.get_formatted_time())
+	points_label.text = "Points:" + str(score)
+	time_label.text = str(timer.get_formatted_time())
