589420ae549133ef1404fad444da5680048295ad
Struckdown <bfleysher@gmail.com>
2020-08-16 14:53:10 -0600

    Pause graphic update

--- src/Scripts/Pause Control.gd
+++ src/Scripts/Pause Control.gd
@@ -1,22 +1,11 @@
 extends Node
 export(NodePath) var top_level_control_node
 onready var top_level_control: Control = (get_node(top_level_control_node) as Control)
-export(NodePath) var continue_button_node
-onready var continue_button: Button = (get_node(continue_button_node) as Button)
-export(NodePath) var main_menu_button_node
-onready var main_menu_button: Button = (get_node(main_menu_button_node) as Button)
-export(NodePath) var application_close_button_node
-onready var application_close_button: Button = (get_node(application_close_button_node) as Button)
-export(NodePath) var mouseover_soundeffect_node
-onready var mouseover_soundeffect: AudioStreamPlayer = (get_node(mouseover_soundeffect_node) as AudioStreamPlayer)
+
 export(String, FILE) var main_menu_scene
 
 export(NodePath) var win_menu_node
 onready var win_menu := get_node(win_menu_node)
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -24,14 +13,7 @@ func _ready():
 	top_level_control.visible=false
 	
 	#Connect button functionality
-	continue_button.connect("pressed", self, "_pause_toggle", [false])
-	main_menu_button.connect("pressed", self, "_main_menu_switch")
-	application_close_button.connect("pressed", self, "_quit_application")
-	
-	#Connect button sound effects
-	continue_button.connect("mouse_entered", self, "_button_mouseover_sfx")
-	main_menu_button.connect("mouse_entered", self, "_button_mouseover_sfx")
-	application_close_button.connect("mouse_entered", self, "_button_mouseover_sfx")	
+	$PauseMenuLayer/CenterContainer/PanelContainer/VBoxContainer/Continue_Btn.connect("pressed", self, "_pause_toggle", [false])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -51,4 +33,20 @@ func _quit_application() -> void:
 	get_tree().quit()
 
 func _button_mouseover_sfx() -> void:
-	mouseover_soundeffect.play()
+	$mouseover_soundeffect.play()
+
+
+func _on_Main_Menu_Btn_pressed():
+	_main_menu_switch()
+
+
+func _on_Exit_Btn_pressed():
+	_quit_application()
+
+
+func _on__Btn_mouse_entered():
+	$mouseover_soundeffect.play()
+
+
+func _on_Btn_mouse_entered():
+	$mouseover_soundeffect.play()
