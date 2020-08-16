18790be71bfbc81324c3cbc8ea1782bf7f8cb93a
David Estes McKnight <belmont95@live.com>
2020-08-16 15:24:00 -0500

    Updating UI

--- src/Scripts/UI.gd
+++ src/Scripts/UI.gd
@@ -7,6 +7,8 @@ export(NodePath) var time_label_node
 onready var time_label := get_node(time_label_node)
 export(NodePath) var points_label_node
 onready var points_label := get_node(points_label_node)
+export(NodePath) var win_menu_node
+onready var win_menu := get_node(win_menu_node)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,5 +21,8 @@ func _process(delta):
 
 
 func update():
-	points_label.text = "Points:" + str(score)
-	time_label.text = str(timer.get_formatted_time())
+	points_label.text = "Tincturings: " + str(score)
+	var time_string = ""
+	if win_menu.game_won:
+		time_string = time_string + "Won at: "
+	time_label.text = time_string + str(timer.get_formatted_time())
